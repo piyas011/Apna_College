14cd6bf019ac1b05adb8ad2cb06b31e703f873fa
Piyas Ahmed <apiyas761@gmail.com>
2026-07-16 20:58:38 +0600

    Learn js with apna college

---
+++ javaScript/script.js
@@ -0,0 +1,30 @@
+let firstCode = "Hello World";
+console.log(firstCode);
+console.log("Piyas Ahmed");
+console.log("I love javaScript");
+console.log("Hello , Welcome to JavaScript");
+
+// JavaScript Variables
+// Variables are containers for data
+
+// let name = "Piyas Ahmed";
+let firstName = "Piyas ";
+let lastName = "Ahmed";
+let fullName = firstName + lastName;
+console.log(fullName);
+let age = 21;
+age = ++age;
+console.log(age);
+let price = 100;
+console.log(price);
+age = --age;
+console.log(age);
+let x = null;
+let y = undefined;
+console.log(x);
+console.log(y);
+////////////////////////
+let isfollow = false;
+console.log(isfollow);
+isfollow = true;
+console.log(isfollow);
