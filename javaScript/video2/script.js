cf8f84bf16414bff00b0b5e09ba548dcb7889000
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 22:17:42 +0600

    learn javaScript

---
+++ javaScript/video2/script.js
@@ -0,0 +1,20 @@
+// This code prints hello world
+console.log("Hello World");
+
+let a = 5;
+let b = 2;
+
+console.log(`a + b = ${a + b}`);
+console.log(`a - b = ${a - b}`);
+console.log(`a * b = ${a * b}`);
+console.log(`a / b = ${a / b}`);
+console.log(`a % b = ${a % b}`);
+console.log(`a ** b = ${a ** b}`);
+
+// Unary Operator
+
+let c = 5;
+let d = 2;
+console.log(`c = ${c} & d = ${d}`);
+c = c + 1; // 6
+console.log(`c = ${c}`);
