f311d413d2df9337f0a3e2fa16e7af48edadfe85
Piyas Ahmed <apiyas761@gmail.com>
2026-07-20 17:14:15 +0600

    task

---
+++ javaScript/video2/task.js
@@ -0,0 +1,16 @@
+// Task 1
+// Print all even numbers from 0 to 100
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+console.log("-----------------------------------");
+// Task 1
+// Print all odd numbers from 0 to 99
+for (let x = 0; x <= 100; x++) {
+  if (x % 2 !== 0) {
+    console.log(x);
+  }
+}
