6a553fd9ebc91393b5028608587a4dede18f475e
Piyas Ahmed <apiyas761@gmail.com>
2026-07-20 16:40:01 +0600

    new file add

--- javaScript/video2/forOfLoop.js
+++ javaScript/video2/forOfLoop.js
@@ -10,12 +10,12 @@
 // console.log(size);
 
 // for in loop
-let student = {
-  name: "Piyas Ahmed",
-  age: 21,
-  cgpa: 5.44,
-};
+// let student = {
+//   name: "Piyas Ahmed",
+//   age: 21,
+//   cgpa: 5.44,
+// };
 
-for (let key in student) {
-  console.log(student[key]);
-}
+// for (let key in student) {
+//   console.log(student[key]);
+// }
