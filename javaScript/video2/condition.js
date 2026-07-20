7e82e88de3adffb300f4bffa449e638a5ade6758
Piyas Ahmed <apiyas761@gmail.com>
2026-07-20 12:52:58 +0600

    new code added

---
+++ javaScript/video2/condition.js
@@ -0,0 +1,94 @@
+// // Conditional Starements
+// let age = 21;
+
+// if (age > 18) {
+//   console.log(`You Can vote`);
+// }
+
+// let mode = "dark";
+// let color;
+
+// //if
+
+// // if (mode === "dark") {
+// //   color = "Black";
+// // }
+// // if (mode === "light") {
+// //   color = "white";
+// // }
+
+// //else
+
+// if (mode === "dark") {
+//   color = "Black";
+// } else {
+//   color = "white";
+// }
+// console.log(color);
+
+// // even of odd
+// let num = 101;
+// if (num % 2 === 0) {
+//   console.log(`${num} = Even Number`);
+// } else {
+//   console.log(`${num} = Odd Number`);
+// }
+
+// // else if
+
+// let number = -121;
+// let result;
+// if (number > 0) {
+//   result = "Positive";
+// } else if (number < 0) {
+//   result = "Negative";
+// } else {
+//   result = "Zero";
+// }
+// console.log(result);
+
+// // ternary operators
+// result = age >= 18 ? "adult" : "not Adult";
+// console.log(result);
+
+// let number10 = -12;
+// let result10 = number10 > 0 ? "Positive" : number10 < 0 ? "nagative" : "zero";
+// console.log(result10);
+
+// switch
+// const expr = "Oranges";
+
+// switch (expr) {
+//   case "Oranges":
+//     console.log("Oranges are $0.59 a pound.");
+//     break;
+//   case "Apples":
+//     console.log("Apples are $0.32 a pound.");
+//     break;
+//   case "Bananas":
+//     console.log("Bananas are $0.48 a pound.");
+//     break;
+//   case "Cherries":
+//     console.log("Cherries are $3.00 a pound.");
+//     break;
+//   case "Mangoes":
+//   case "Papayas":
+//     console.log("Mangoes and papayas are $2.79 a pound.");
+//     break;
+//   default:
+//     console.log(`Sorry, we are out of ${expr}.`);
+// }
+
+// console.log("Is there anything else you'd like?");
+
+// alert("Hello");
+// let name = prompt("Hello");
+// console.log(name);
+
+// let num = parseInt(prompt("Enter a Number"));
+
+// if (num % 5 === 0) {
+//   console.log(`${num} is a multiple of 5`);
+// } else {
+//   console.log(`${num} is not multiple of 5`);
+// }
