9a005c2c86b897d8c257e9f40b9518009fccd396
Piyas Ahmed <apiyas761@gmail.com>
2026-07-23 21:09:56 +0600

    javaScript Function

---
+++ javaScript/video2/function.js
@@ -0,0 +1,20 @@
+// Function -> 2 number,sum
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// sum(3, 3);
+
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
+// // console.log(sum(34, 45));
+// const result = sum(34, 45);
+// console.log(result);
+
+const sayHello = (a, b) => {
+  return a + b;
+};
+
+console.log(sayHello(3, 3));
