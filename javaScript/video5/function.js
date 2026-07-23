a26721acca92044807bce733d50904f8c88863a9
Piyas Ahmed <apiyas761@gmail.com>
2026-07-23 21:14:50 +0600

    new follder add

---
+++ javaScript/video5/function.js
@@ -0,0 +1,29 @@
+// Function -> 2 number,sum
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// sum(3, 3);
+
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
+// // console.log(sum(34, 45));
+// const result = sum(34, 45);
+// console.log(result);
+
+// const sayHello = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(sayHello(3, 3));
+
+//////////////////////////
+const sumOfNumber = (a, b) => {
+  const sum = a + b;
+  return sum;
+};
+
+const result = sumOfNumber(12, 23);
+console.log(result);
